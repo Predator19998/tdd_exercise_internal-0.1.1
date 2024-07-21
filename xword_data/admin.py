bb16913ed1cf2b1d5ec61bec1d437214dc9e5e00
Jerry Sivaram Reddy Rajasimha Reddy <raja.simha0@gmail.com>
2024-07-21 11:12:32 -0700

    Created admin, urls and final touches

--- xword_data/admin.py
+++ xword_data/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Puzzle, Entry, Clue
 
 # Register your models here.
+@admin.register(Puzzle)
+class PuzzleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'byline', 'publisher')
+    search_fields = ('title', 'byline', 'publisher')
+    list_filter = ('date', 'publisher')
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('entry_text',)
+    search_fields = ('entry_text',)
+
+@admin.register(Clue)
+class ClueAdmin(admin.ModelAdmin):
+    list_display = ('clue_text', 'entry', 'puzzle', 'theme')
+    search_fields = ('clue_text', 'entry__entry_text', 'puzzle__title')
+    list_filter = ('theme', 'puzzle')
---
+++ xword_data/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import drill_view, answer_view
+
+urlpatterns = [
+    path('', drill_view, name='xword-drill'),
+    path('answer/<int:clue_id>/', answer_view, name='xword-answer'),
+]
